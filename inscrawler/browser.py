446d10ca9986d63260d3c0d393aea0e4aafca9be
ULUL AZMI <33614016+azmi155@users.noreply.github.com>
2020-01-04 21:51:25 +0800

    Add files via upload

--- inscrawler/browser.py
+++ inscrawler/browser.py
@@ -1,31 +1,36 @@
 import os
+
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.keys import Keys
+
 from .utils import randmized_sleep
 
 
 class Browser:
     def __init__(self, has_screen):
         dir_path = os.path.dirname(os.path.realpath(__file__))
-        service_args = ['--ignore-ssl-errors=true']
+        service_args = ["--ignore-ssl-errors=true"]
         chrome_options = Options()
         if not has_screen:
             chrome_options.add_argument("--headless")
         chrome_options.add_argument("--start-maximized")
         chrome_options.add_argument("--no-sandbox")
         self.driver = webdriver.Chrome(
-            executable_path='%s/bin/chromedriver' % dir_path,
+            executable_path="%s/bin/chromedriver" % dir_path,
             service_args=service_args,
-            chrome_options=chrome_options)
+            chrome_options=chrome_options,
+        )
         self.driver.implicitly_wait(5)
 
     @property
     def page_height(self):
-        return self.driver.execute_script('return document.body.scrollHeight')
+        return self.driver.execute_script("return document.body.scrollHeight")
 
     def get(self, url):
         self.driver.get(url)
@@ -50,15 +55,16 @@ class Browser:
         except NoSuchElementException:
             return None
 
-
-
     def find(self, css_selector, elem=None, waittime=0):
         obj = elem or self.driver
 
-        if waittime:
-            WebDriverWait(obj, waittime).until(
-                EC.presence_of_element_located((By.CSS_SELECTOR, css_selector))
-            )
+        try:
+            if waittime:
+                WebDriverWait(obj, waittime).until(
+                    EC.presence_of_element_located((By.CSS_SELECTOR, css_selector))
+                )
+        except TimeoutException:
+            return None
 
         try:
             return obj.find_elements(By.CSS_SELECTOR, css_selector)
@@ -66,19 +72,27 @@ class Browser:
             return None
 
     def scroll_down(self, wait=0.3):
-        self.driver.execute_script(
-            'window.scrollTo(0, document.body.scrollHeight)')
+        self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
         randmized_sleep(wait)
 
     def scroll_up(self, offset=-1, wait=2):
-        if (offset == -1):
-            self.driver.execute_script('window.scrollTo(0, 0)')
+        if offset == -1:
+            self.driver.execute_script("window.scrollTo(0, 0)")
         else:
-            self.driver.execute_script('window.scrollBy(0, -%s)' % offset)
+            self.driver.execute_script("window.scrollBy(0, -%s)" % offset)
         randmized_sleep(wait)
 
     def js_click(self, elem):
         self.driver.execute_script("arguments[0].click();", elem)
+
+    def open_new_tab(self, url):
+        self.driver.execute_script("window.open('%s');" %url)
+        self.driver.switch_to.window(self.driver.window_handles[1])
+
+    def close_current_tab(self):
+        self.driver.close()
+
+        self.driver.switch_to.window(self.driver.window_handles[0])
 
     def __del__(self):
         try:
